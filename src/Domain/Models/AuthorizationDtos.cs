a99f3ccf3c4c1af3b574bf1494760744291a4858
Giorgi Gogoladze <ggogoladze@Giorgis-MacBook-Air.local>
2026-01-06 00:20:41 +0400

    Initial commit

---
+++ src/Domain/Models/AuthorizationDtos.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
+
+public record RegisterDto
+{
+    [Required(ErrorMessage = "Username is required.")]
+    public string Username { get; init; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string Email { get; init; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+    public string Password { get; init; }
+}
+
+public record LoginDto
+{
+    [Required(ErrorMessage = "Username is required.")]
+    public string Username { get; init; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; init; }
+}
+
+
+public record LoginResponseDto(Guid Id,string Username, string Token);
+public record ResetPasswordDto(string Email, string Token, string NewPassword);
+
